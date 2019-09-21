ac5485eee0cd751ad5b69b6de61eabce963fb8a0
Anmol17Agarwal <anmol.agarwal_iot18@gla.ac.in>
2019-09-21 23:36:08 +0530

    import Scanner class

--- src/Books.java
+++ src/Books.java
@@ -1,29 +1,41 @@
+import java.util.Scanner;
 public class Books {
     private String bookName;
     private String authorName;
     private long isbn;
+
     public String getBookName() {
         return bookName;
     }
+
     public void setBookName(String bookName) {
         this.bookName = bookName;
     }
-    public String getAuthorName(){
+
+    public String getAuthorName() {
         return authorName;
     }
+
     public void setAuthorName(String authorName) {
         this.authorName = authorName;
     }
+
     public long getIsbn() {
         return isbn;
     }
+
     public void setIsbn(long isbn) {
         this.isbn = isbn;
     }
+
     public Books(String bookName, String authorName, long isbn) {
-        this.bookName=bookName;
-        this.authorName=authorName;
-        this.isbn=isbn;
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.isbn = isbn;
     }
 
+    @Override
+    public String toString() {
+        return "Book Name:\t" + getBookName() + "\nAuthor Name:\t" + getAuthorName() + "\nISBN:\t" + getIsbn();
+    }
 }
