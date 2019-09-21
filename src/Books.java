5e549e5f4e5bab8c1388a6fec567fc2a980891b8
Anmol17Agarwal <anmol.agarwal_iot18@gla.ac.in>
2019-09-21 23:41:33 +0530

    a is storing value by user about no of books

--- src/Books.java
+++ src/Books.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class Magic {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("enter numbers of Books");
+        int a = scanner.nextInt();
     }
 }
 public class Books {
